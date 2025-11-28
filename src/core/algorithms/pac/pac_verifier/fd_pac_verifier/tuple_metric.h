2935d0d823f89f5ca931defd37517a6fb7773747
Senichenkov <Petua41@yandex.ru>
2025-11-28 21:15:39 +0300

    Implement FD-PAC-related base model classes
    Implement base model classes, that are needed for FD PAC validation
    (and, sometimes, for UCC PAC validation)

---
+++ src/core/algorithms/pac/pac_verifier/fd_pac_verifier/tuple_metric.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <vector>
+
+#include "core/algorithms/pac/model/tuple.h"
+#include "core/algorithms/pac/pac_verifier/fd_pac_verifier/column_metric.h"
+
+namespace algos::pac_verifier {
+/// @brief Holds @c ColumnMetrics for all columns and calculates distance between tuples in terms
+/// of FD PAC verifier, i. e. as maximum distance between values
+class TupleMetric {
+private:
+    std::vector<ColumnMetric> lhs_metrics_;
+    std::vector<ColumnMetric> rhs_metrics_;
+
+    double Distance(pac::model::Tuple const& a, pac::model::Tuple const& b,
+                    std::vector<ColumnMetric> const& metrics) const {
+        assert(a.size() == metrics.size());
+        assert(b.size() == metrics.size());
+
+        double max_dist = 0;
+        for (std::size_t col_num = 0; col_num < metrics.size(); ++col_num) {
+            max_dist = std::max(max_dist, metrics[col_num].Dist(a[col_num], b[col_num]));
+        }
+        return max_dist;
+    }
+
+public:
+    TupleMetric(std::vector<ColumnMetric>&& lhs_metrics, std::vector<ColumnMetric>&& rhs_metrics)
+        : lhs_metrics_(std::move(lhs_metrics)), rhs_metrics_(std::move(rhs_metrics)) {}
+
+    double LhsDist(pac::model::Tuple const& a, pac::model::Tuple const& b) const {
+        return Distance(a, b, lhs_metrics_);
+    }
+
+    double LhsDistOnColumn(pac::model::Tuple const& a, pac::model::Tuple const& b,
+                           std::size_t col_num) const {
+        return lhs_metrics_[col_num].Dist(a[col_num], b[col_num]);
+    }
+
+    double RhsDist(pac::model::Tuple const& a, pac::model::Tuple const& b) const {
+        return Distance(a, b, rhs_metrics_);
+    }
+
+    double RhsDistOnColumn(pac::model::Tuple const& a, pac::model::Tuple const& b,
+                           std::size_t col_num) const {
+        return rhs_metrics_[col_num].Dist(a[col_num], b[col_num]);
+    }
+};
+}  // namespace algos::pac_verifier
---
+++ src/core/algorithms/pac/pac_verifier/fd_pac_verifier/tuple_pair.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstddef>
+
+namespace algos::pac_verifier {
+/// @brief Holds info about pair of tuples: indices of tuples and distance between them.
+struct TuplePair {
+    std::size_t first_idx;
+    std::size_t second_idx;
+    double lhs_dist;
+    double rhs_dist;
+};
+}  // namespace algos::pac_verifier
