878601c73430e94a1c8e756dce9d18dbb947f549
Petr Senichenkov <petua@du-bois-reymond.ru-central1.internal>
2026-06-12 18:58:58 +0000

    Add Domain PAC phases results

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -3,4 +3,4 @@ set -e
 echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 sudo systemctl stop unattended-upgrades apt-daily apt-daily.timer
 sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
-taskset -c 1 uv run ~/Desbordante/src/tests/benchmark/benchmarker/main.py results -n50 -d'domain-pac-medium-iowas'
+taskset -c 1 uv run ~/Desbordante/src/tests/benchmark/benchmarker/main.py logs -n35 -d'domain-pac-phases-iowa-50k'
