Desbordante/desbordante-core

Implement base PAC model classes
Implement TupleType class to calculate distances between fixed-sized tuples of values.
Implement IDomain class to represent an ordered domain in a metric space
of values.
Implement default domain classes: Ball and Parallelepiped.

Implement PAC and DomainPAC classes.

src/core/algorithms/pac/model/default_domains/ball.cpp
@@ -1,0 +1,39 @@
+#include "core/algorithms/pac/model/default_domains/ball.h"
+
+#include <cmath>
+#include <cstddef>
+#include <sstream>
+#include <string>
+
+#include "core/algorithms/pac/model/tuple.h"
+#include "core/algorithms/pac/model/tuple_type.h"
+
+namespace pac::model {
+double Ball::EuclideanDist(Tuple const& x, Tuple const& y) const {
+    double dist = 0;
+    for (std::size_t i = 0; i < metrizable_types_.size(); ++i) {
+        auto coord_dist = DistBetweenBytes(i, x[i], y[i]);
+        dist += coord_dist * coord_dist;
+    }
+    return std::sqrt(dist);
+}
+
+Ball::~Ball() {
+    for (std::size_t i = 0; i < center_.size(); ++i) {
+        auto const& destroy = destructors_[i];
+        if (destroy) {
+            destroy(center_[i]);
+        } else {
+            delete[] center_[i];
+        }
+    }
+}
+
+std::string Ball::ToString() const {
+    // tuple_type_->ValueToString() would be clearer, but domain isn't guaranteed to be instantiated
+    // with types by here.
+    std::ostringstream oss;
+    oss << "B(" << StringValueToString(center_str_) << ", " << radius_ << ')';
+    return oss.str();
+}
+}  // namespace pac::model

src/core/algorithms/pac/model/default_domains/metric_based_domain.cpp
@@ -1,0 +1,82 @@
+#include "core/algorithms/pac/model/default_domains/metric_based_domain.h"
+
+#include <algorithm>
+#include <cstddef>
+#include <functional>
+#include <iterator>
+#include <limits>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "core/algorithms/pac/model/tuple.h"
+#include "core/config/exceptions.h"
+#include "core/model/types/imetrizable_type.h"
+
+namespace pac::model {
+using namespace ::model;
+
+void MetricBasedDomain::ThrowIfEmpty() const {
+    if (metrizable_types_.empty()) {
+        throw std::runtime_error(
+                "Metric-based domain must be instantiated with types (SetTypes) before use");
+    }
+}
+
+void MetricBasedDomain::ValidateTypes(std::vector<Type const*> const& types) {
+    metrizable_types_ = std::vector<IMetrizableType const*>(types.size());
+    for (std::size_t i = 0; i < types.size(); ++i) {
+        auto const* metrizable_type = dynamic_cast<IMetrizableType const*>(types[i]);
+        if (!metrizable_type) {
+            throw config::ConfigurationError("Cannot use metric-based domain, because column #" +
+                                             std::to_string(i) + " has type " +
+                                             types[i]->ToString() + ", which is not metrizable");
+        }
+        metrizable_types_[i] = metrizable_type;
+    }
+
+    // All leveling coefficients that are not specified are 1
+    leveling_coeffs_.insert(leveling_coeffs_.end(), types.size() - leveling_coeffs_.size(), 1);
+}
+
+double MetricBasedDomain::DistBetweenBytes(std::size_t type_num, std::byte const* x,
+                                           std::byte const* y) const {
+    if (x == nullptr || y == nullptr) {
+        return dist_from_null_is_infty_ ? std::numeric_limits<double>::infinity() : 0;
+    }
+    return metrizable_types_[type_num]->Dist(x, y) * leveling_coeffs_[type_num];
+}
+
+std::vector<std::byte const*> MetricBasedDomain::AllocateValues(
+        std::vector<std::string> const& str_values) const {
+    if (str_values.size() != metrizable_types_.size()) {
+        throw config::ConfigurationError(
+                "Metric-based domain must be initialized with the same number of values as the "
+                "number of columns used");
+    }
+
+    std::vector<std::byte const*> values(metrizable_types_.size());
+    for (std::size_t i = 0; i < metrizable_types_.size(); ++i) {
+        auto const& type = metrizable_types_[i];
+        auto* ptr = type->Allocate();
+        type->ValueFromStr(ptr, str_values[i]);
+        values[i] = ptr;
+    }
+    return values;
+}
+
+std::vector<Type::Destructor> MetricBasedDomain::GetDestructors() const {
+    std::vector<Type::Destructor> destructors;
+    destructors.reserve(metrizable_types_.size());
+    std::ranges::transform(metrizable_types_, std::back_inserter(destructors),
+                           std::mem_fn(&::model::Type::GetDestructor));
+    return destructors;
+}
+
+double MetricBasedDomain::DistFromDomain(Tuple const& value) const {
+    assert(value.size() == metrizable_types_.size());
+    ThrowIfEmpty();
+
+    return DistFromDomainInternal(value);
+}
+}  // namespace pac::model

src/core/algorithms/pac/model/default_domains/parallelepiped.cpp
@@ -1,0 +1,71 @@
+#include "core/algorithms/pac/model/default_domains/parallelepiped.h"
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <sstream>
+#include <string>
+
+#include "core/algorithms/pac/model/tuple.h"
+#include "core/algorithms/pac/model/tuple_type.h"
+#include "core/config/exceptions.h"
+#include "core/model/types/builtin.h"
+
+namespace pac::model {
+double Parallelepiped::DistFromDomainInternal(Tuple const& value) const {
+    // d(x, D) = min{rho(x, y)}, where y in D, rho is Chebyshev dist:
+    //   rho(x, y) = max{|x[i] - y[i]|}, i = 1, ..., n
+    // For parallelepiped only points on bounds need to be considered, i. e. y in dD
+    // For each dimension bounds are first_[i] and last_[i]
+    // Distance from bounds on one dimension is min{|x[i] - first_[i]|, |x[i] - last_[i]|}
+    // Thus, distance becomes
+    //   d(x, D) = max{min{|x[i] - first_[i]|, |x[i] - last_[i]|}}, i = 1, ..., n
+    double max_dist = 0;
+    for (std::size_t i = 0; i < metrizable_types_.size(); ++i) {
+        double one_dim_dist = 0;
+        if (tuple_type_->CompareBytes(i, value[i], first_[i]) == ::model::CompareResult::kLess) {
+            one_dim_dist = DistBetweenBytes(i, value[i], first_[i]);
+        } else if (tuple_type_->CompareBytes(i, last_[i], value[i]) ==
+                   ::model::CompareResult::kLess) {
+            one_dim_dist = DistBetweenBytes(i, value[i], last_[i]);
+        }
+        max_dist = std::max(max_dist, one_dim_dist);
+    }
+    return max_dist;
+}
+
+void Parallelepiped::ConvertValues() {
+    first_ = AllocateValues(first_str_);
+    last_ = AllocateValues(last_str_);
+    destructors_ = GetDestructors();
+
+    // Check that first_ <= last_
+    for (std::size_t i = 0; i < metrizable_types_.size(); ++i) {
+        if (tuple_type_->CompareBytes(i, first_[i], last_[i]) == ::model::CompareResult::kGreater) {
+            throw config::ConfigurationError(
+                    "Lower bound of Parallelepiped must be less or equal than the upper bound.");
+        }
+    }
+}
+
+Parallelepiped::~Parallelepiped() {
+    for (std::size_t i = 0; i < first_.size(); ++i) {
+        auto const& destroy = destructors_[i];
+        if (destroy) {
+            destroy(first_[i]);
+            destroy(last_[i]);
+        } else {
+            delete[] first_[i];
+            delete[] last_[i];
+        }
+    }
+}
+
+std::string Parallelepiped::ToString() const {
+    // tuple_type_->ValueToString() would be clearer, but domain isn't guaranteed to be instantiated
+    // with types by here.
+    std::ostringstream oss;
+    oss << '[' << StringValueToString(first_str_) << ", " << StringValueToString(last_str_) << ']';
+    return oss.str();
+}
+}  // namespace pac::model

src/core/algorithms/pac/model/tuple_type.h
@@ -1,0 +1,49 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "core/algorithms/pac/model/tuple.h"
+#include "core/model/types/builtin.h"
+#include "core/model/types/type.h"
+
+namespace pac::model {
+/// @brief Provides operations for fixed-size tuples of values on a fixed set of attributes
+class TupleType {
+private:
+    std::vector<::model::Type const*> types_;
+
+public:
+    TupleType(std::vector<::model::Type const*>&& types) : types_(std::move(types)) {}
+
+    /// @brief Convert @c std::byte* of type @c types[type_num] to string.
+    /// Consider using this function rather than directly calling @c types[type_num]->ValueToString,
+    /// because it correctly handles NULLs
+    std::string ByteToString(std::size_t type_num, std::byte const* value) const {
+        assert(type_num < types_.size());
+
+        if (value == nullptr) {
+            return "NULL";
+        }
+        return types_[type_num]->ValueToString(value);
+    }
+
+    /// @brief Compare two @c std::byte pointers of type @c types[type_num].
+    /// Consider using this function rather than directly call @c types[type_num]->Compare, because
+    /// it correctly handles NULLs (NULL is considered less than any value).
+    ::model::CompareResult CompareBytes(std::size_t const type_num, std::byte const* x,
+                                        std::byte const* y);
+
+    std::string ValueToString(Tuple const& value) const;
+
+    std::vector<::model::Type const*> const& GetTypes() const {
+        return types_;
+    }
+};
+
+/// @brief Converts vector of strings of the same format as ComparableTupleType::ValueToString()
+/// does
+std::string StringValueToString(std::vector<std::string> const& strings);
+}  // namespace pac::model

p-senichenkov/Desbordante

src/core/algorithms/pac/model/default_domains/ball.cpp
@@ -1,0 +1,39 @@
+#include "core/algorithms/pac/model/default_domains/ball.h"
+
+#include <cmath>
+#include <cstddef>
+#include <sstream>
+#include <string>
+
+#include "core/algorithms/pac/model/tuple.h"
+#include "core/algorithms/pac/model/tuple_type.h"
+
+namespace pac::model {
+double Ball::EuclideanDist(Tuple const& x, Tuple const& y) const {
+    double dist = 0;
+    for (std::size_t i = 0; i < metrizable_types_.size(); ++i) {
+        auto coord_dist = ComponentDistance(i, x[i], y[i]);
+        dist += coord_dist * coord_dist;
+    }
+    return std::sqrt(dist);
+}
+
+Ball::~Ball() {
+    for (std::size_t i = 0; i < center_.size(); ++i) {
+        auto const& destroy = destructors_[i];
+        if (destroy) {
+            destroy(center_[i]);
+        } else {
+            delete[] center_[i];
+        }
+    }
+}
+
+std::string Ball::ToString() const {
+    // tuple_type_->ValueToString() would be clearer, but domain isn't guaranteed to be instantiated
+    // with types by here.
+    std::ostringstream oss;
+    oss << "B(" << StringValueToString(center_str_) << ", " << radius_ << ')';
+    return oss.str();
+}
+}  // namespace pac::model

src/core/algorithms/pac/model/default_domains/metric_based_domain.cpp
@@ -1,0 +1,81 @@
+#include "core/algorithms/pac/model/default_domains/metric_based_domain.h"
+
+#include <algorithm>
+#include <cstddef>
+#include <functional>
+#include <iterator>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "core/algorithms/pac/model/tuple.h"
+#include "core/config/exceptions.h"
+#include "core/model/types/imetrizable_type.h"
+
+namespace pac::model {
+using namespace ::model;
+
+void MetricBasedDomain::ThrowIfEmpty() const {
+    if (metrizable_types_.empty()) {
+        throw std::runtime_error(
+                "Metric-based domain must be instantiated with types (SetTypes) before use");
+    }
+}
+
+void MetricBasedDomain::ValidateTypes(std::vector<Type const*> const& types) {
+    metrizable_types_ = std::vector<IMetrizableType const*>(types.size());
+    for (std::size_t i = 0; i < types.size(); ++i) {
+        auto const* metrizable_type = dynamic_cast<IMetrizableType const*>(types[i]);
+        if (!metrizable_type) {
+            throw config::ConfigurationError("Cannot use metric-based domain, because column #" +
+                                             std::to_string(i) + " has type " +
+                                             types[i]->ToString() + ", which is not metrizable");
+        }
+        metrizable_types_[i] = metrizable_type;
+    }
+
+    // All leveling coefficients that are not specified are 1
+    leveling_coeffs_.insert(leveling_coeffs_.end(), types.size() - leveling_coeffs_.size(), 1);
+}
+
+double MetricBasedDomain::ComponentDistance(std::size_t component_idx, std::byte const* x,
+                                            std::byte const* y) const {
+    if (x == nullptr || y == nullptr) {
+        return 0;
+    }
+    return metrizable_types_[component_idx]->Dist(x, y) * leveling_coeffs_[component_idx];
+}
+
+std::vector<std::byte const*> MetricBasedDomain::AllocateValues(
+        std::vector<std::string> const& str_values) const {
+    if (str_values.size() != metrizable_types_.size()) {
+        throw config::ConfigurationError(
+                "Metric-based domain must be initialized with the same number of values as the "
+                "number of columns used");
+    }
+
+    std::vector<std::byte const*> values(metrizable_types_.size());
+    for (std::size_t i = 0; i < metrizable_types_.size(); ++i) {
+        auto const& type = metrizable_types_[i];
+        auto* ptr = type->Allocate();
+        type->ValueFromStr(ptr, str_values[i]);
+        values[i] = ptr;
+    }
+    return values;
+}
+
+std::vector<Type::Destructor> MetricBasedDomain::GetDestructors() const {
+    std::vector<Type::Destructor> destructors;
+    destructors.reserve(metrizable_types_.size());
+    std::ranges::transform(metrizable_types_, std::back_inserter(destructors),
+                           std::mem_fn(&::model::Type::GetDestructor));
+    return destructors;
+}
+
+double MetricBasedDomain::DistFromDomain(Tuple const& value) const {
+    assert(value.size() == metrizable_types_.size());
+    ThrowIfEmpty();
+
+    return DistFromDomainInternal(value);
+}
+}  // namespace pac::model

src/core/algorithms/pac/model/default_domains/parallelepiped.cpp
@@ -1,0 +1,73 @@
+#include "core/algorithms/pac/model/default_domains/parallelepiped.h"
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <sstream>
+#include <string>
+
+#include "core/algorithms/pac/model/tuple.h"
+#include "core/algorithms/pac/model/tuple_type.h"
+#include "core/config/exceptions.h"
+#include "core/model/types/builtin.h"
+
+namespace pac::model {
+double Parallelepiped::DistFromDomainInternal(Tuple const& value) const {
+    // d(x, D) = min{rho(x, y)}, where y in D, rho is Chebyshev dist:
+    //   rho(x, y) = max{|x[i] - y[i]|}, i = 1, ..., n
+    // For parallelepiped only points on bounds need to be considered, i. e. y in dD
+    // For each dimension bounds are first_[i] and last_[i]
+    // Distance from bounds on one dimension is min{|x[i] - first_[i]|, |x[i] - last_[i]|}
+    // Thus, distance becomes
+    //   d(x, D) = max{min{|x[i] - first_[i]|, |x[i] - last_[i]|}}, i = 1, ..., n
+    double max_dist = 0;
+    for (std::size_t i = 0; i < metrizable_types_.size(); ++i) {
+        double one_dim_dist = 0;
+        if (tuple_type_->CompareComponents(i, value[i], first_[i]) ==
+            ::model::CompareResult::kLess) {
+            one_dim_dist = ComponentDistance(i, value[i], first_[i]);
+        } else if (tuple_type_->CompareComponents(i, last_[i], value[i]) ==
+                   ::model::CompareResult::kLess) {
+            one_dim_dist = ComponentDistance(i, value[i], last_[i]);
+        }
+        max_dist = std::max(max_dist, one_dim_dist);
+    }
+    return max_dist;
+}
+
+void Parallelepiped::ConvertValues() {
+    first_ = AllocateValues(first_str_);
+    last_ = AllocateValues(last_str_);
+    destructors_ = GetDestructors();
+
+    // Check that first_ <= last_
+    for (std::size_t i = 0; i < metrizable_types_.size(); ++i) {
+        if (tuple_type_->CompareComponents(i, first_[i], last_[i]) ==
+            ::model::CompareResult::kGreater) {
+            throw config::ConfigurationError(
+                    "Lower bound of Parallelepiped must be less or equal than the upper bound.");
+        }
+    }
+}
+
+Parallelepiped::~Parallelepiped() {
+    for (std::size_t i = 0; i < first_.size(); ++i) {
+        auto const& destroy = destructors_[i];
+        if (destroy) {
+            destroy(first_[i]);
+            destroy(last_[i]);
+        } else {
+            delete[] first_[i];
+            delete[] last_[i];
+        }
+    }
+}
+
+std::string Parallelepiped::ToString() const {
+    // tuple_type_->ValueToString() would be clearer, but domain isn't guaranteed to be instantiated
+    // with types by here.
+    std::ostringstream oss;
+    oss << '[' << StringValueToString(first_str_) << ", " << StringValueToString(last_str_) << ']';
+    return oss.str();
+}
+}  // namespace pac::model

src/core/algorithms/pac/model/tuple_type.h
@@ -1,0 +1,50 @@
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "core/algorithms/pac/model/tuple.h"
+#include "core/model/types/builtin.h"
+#include "core/model/types/type.h"
+
+namespace pac::model {
+/// @brief Provides operations for fixed-size tuples of values on a fixed set of attributes
+class TupleType {
+private:
+    std::vector<::model::Type const*> types_;
+
+public:
+    TupleType(std::vector<::model::Type const*>&& types) : types_(std::move(types)) {}
+
+    /// @brief Convert @c std::byte* of type @c types[type_num] to string.
+    /// Consider using this function rather than directly calling @c types[type_num]->ValueToString,
+    /// because it correctly handles NULLs
+    std::string ByteToString(std::size_t type_num, std::byte const* value) const {
+        assert(type_num < types_.size());
+
+        if (value == nullptr) {
+            return "NULL";
+        }
+        return types_[type_num]->ValueToString(value);
+    }
+
+    /// @brief Compare two @c std::byte pointers of type @c types[type_num].
+    /// Consider using this function rather than directly call @c types[type_num]->Compare, because
+    /// it correctly handles NULLs (NULL is considered less than any value).
+    ::model::CompareResult CompareComponents(std::size_t const component_idx, std::byte const* x,
+                                             std::byte const* y);
+
+    std::string ValueToString(Tuple const& value) const;
+
+    std::vector<::model::Type const*> const& GetTypes() const {
+        return types_;
+    }
+};
+
+/// @brief Converts vector of strings of the same format as ComparableTupleType::ValueToString()
+/// does
+std::string StringValueToString(std::vector<std::string> const& strings);
+}  // namespace pac::model