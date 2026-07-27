Desbordante/desbordante-core

Implement bindings for custom metrics

src/python_bindings/py_util/opt_to_py.cpp
@@ -14,6 +14,9 @@
 #include "core/algorithms/md/hymd/enums.h"
 #include "core/algorithms/metric/enums.h"
 #include "core/algorithms/od/fastod/od_ordering.h"
+#include "core/config/custom_metric/custom_metric/type.h"
+#include "core/config/custom_metric/custom_metrics/type.h"
+#include "core/config/custom_metric/custom_vector_metric/type.h"
 #include "core/config/custom_random_seed/type.h"
 #include "core/config/equal_nulls/type.h"
 #include "core/config/error/type.h"
@@ -50,6 +53,9 @@ std::unordered_map<std::type_index, ConvFunction> const kConverters{
         normal_conv_pair<config::IndicesType>,
         normal_conv_pair<model::DDString>,
         normal_conv_pair<model::Gdd>,
+        normal_conv_pair<config::CustomMetricType>,
+        normal_conv_pair<config::CustomMetricsType>,
+        normal_conv_pair<config::CustomVectorMetricType>,
         enum_conv_pair<algos::metric::MetricAlgo>,
         enum_conv_pair<algos::metric::Metric>,
         enum_conv_pair<model::InputFormatType>,

src/python_bindings/data/py_custom_metrics.cpp
@@ -1,0 +1,25 @@
+#include "python_bindings/data/py_custom_metrics.h"
+
+#include <pybind11/pybind11.h>
+
+#include <cassert>
+#include <memory>
+
+#include <pybind11/pytypes.h>
+
+#include "core/util/custom_metric/custom_metric.h"
+#include "core/util/custom_metric/custom_vector_metric.h"
+
+namespace py = pybind11;
+
+namespace python_bindings {
+void BindCustomMetrics(pybind11::module_& main_module) {
+    auto metrics_module = main_module.def_submodule("metrics");
+
+    // These classes are used only in get_opts
+    py::class_<util::ICustomMetric, std::shared_ptr<util::ICustomMetric>>(metrics_module,
+                                                                          "CustomMetric");
+    py::class_<util::ICustomVectorMetric, std::shared_ptr<util::ICustomVectorMetric>>(
+            metrics_module, "CustomVectorMetrics");
+}
+}  // namespace python_bindings

src/python_bindings/py_util/py_to_any.cpp
@@ -1,6 +1,8 @@
 #include <pybind11/pybind11.h>
 
+#include <algorithm>
 #include <functional>
+#include <memory>
 #include <sstream>
 #include <stdexcept>
 #include <string>
@@ -8,6 +10,7 @@
 
 #include <boost/any.hpp>
 #include <boost/core/demangle.hpp>
+#include <pybind11/pytypes.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl/filesystem.h>
 
@@ -23,6 +26,9 @@
 #include "core/algorithms/md/md_verifier/column_similarity_classifier.h"
 #include "core/algorithms/metric/enums.h"
 #include "core/algorithms/od/fastod/od_ordering.h"
+#include "core/config/custom_metric/custom_metric/type.h"
+#include "core/config/custom_metric/custom_metrics/type.h"
+#include "core/config/custom_metric/custom_vector_metric/type.h"
 #include "core/config/error_measure/type.h"
 #include "core/config/exceptions.h"
 #include "core/config/tabular_data/input_table_type.h"
@@ -32,6 +38,7 @@
 #include "core/parser/sequence_parser/file_sequence_parser.h"
 #include "core/util/enum_to_available_values.h"
 #include "core/util/enum_to_str.h"
+#include "python_bindings/data/py_custom_metrics.h"
 #include "python_bindings/py_util/create_dataframe_reader.h"
 #include "python_bindings/py_util/iterable_sequence_stream.h"
 
@@ -193,6 +200,30 @@ boost::any StringVectorToAny(std::string_view option_name, py::handle obj) {
     return out;
 }
 
+boost::any CustomMetricToAny(std::string_view, py::handle obj) {
+    return config::CustomMetricType{
+            new python_bindings::PyCustomMetric(py::reinterpret_borrow<py::object>(obj))};
+}
+
+boost::any CustomMetricsToAny(std::string_view option_name, py::handle obj) {
+    auto metric_handles = CastAndReplaceCastError<std::vector<py::handle>>(option_name, obj);
+    config::CustomMetricsType result(metric_handles.size());
+    std::ranges::transform(metric_handles, result.begin(),
+                           [](py::handle handle) -> config::CustomMetricType {
+                               if (handle.is_none()) {
+                                   return nullptr;
+                               }
+                               return std::make_shared<python_bindings::PyCustomMetric>(
+                                       py::reinterpret_borrow<py::object>(handle));
+                           });
+    return result;
+}
+
+boost::any CustomVectorMetricToAny(std::string_view, py::handle obj) {
+    return config::CustomVectorMetricType{
+            new python_bindings::PyCustomVectorMetric(py::reinterpret_borrow<py::object>(obj))};
+}
+
 std::unordered_map<std::type_index, ConvFunc> const kConverters{
         kNormalConvPair<bool>,
         kNormalConvPair<double>,
@@ -234,6 +265,9 @@ std::unordered_map<std::type_index, ConvFunc> const kConverters{
         kNormalConvPair<std::pair<std::string, std::string>>,
         kNormalConvPair<std::vector<std::string>>,
         kNormalConvPair<std::unordered_map<std::string, std::vector<unsigned int>>>,
+        {typeid(config::CustomMetricType), CustomMetricToAny},
+        {typeid(config::CustomMetricsType), CustomMetricsToAny},
+        {typeid(config::CustomVectorMetricType), CustomVectorMetricToAny},
 };
 
 }  // namespace

p-senichenkov/Desbordante

src/python_bindings/py_util/opt_to_py.cpp
@@ -17,6 +17,9 @@
 #include "core/algorithms/nar/des/enums.h"
 #include "core/algorithms/od/fastod/od_ordering.h"
 #include "core/algorithms/pac/model/idomain.h"
+#include "core/config/custom_metric/custom_metric/type.h"
+#include "core/config/custom_metric/custom_metrics/type.h"
+#include "core/config/custom_metric/custom_vector_metric/type.h"
 #include "core/config/custom_random_seed/type.h"
 #include "core/config/equal_nulls/type.h"
 #include "core/config/error/type.h"
@@ -57,6 +60,9 @@ std::unordered_map<std::type_index, ConvFunction> const kConverters{
         kNormalConvPair<model::DDString>,
         kNormalConvPair<model::Gdd>,
         kNormalConvPair<std::shared_ptr<pac::model::IDomain>>,
+        kNormalConvPair<config::CustomMetricType>,
+        kNormalConvPair<config::CustomMetricsType>,
+        kNormalConvPair<config::CustomVectorMetricType>,
         kEnumConvPair<algos::metric::MetricAlgo>,
         kEnumConvPair<algos::metric::Metric>,
         kEnumConvPair<model::InputFormatType>,

src/python_bindings/py_util/py_custom_metrics.cpp
@@ -1,0 +1,22 @@
+#include "python_bindings/py_util/py_custom_metrics.h"
+
+#include <pybind11/pybind11.h>
+
+#include <memory>
+
+#include "core/util/custom_metric/custom_metric.h"
+#include "core/util/custom_metric/custom_vector_metric.h"
+
+namespace py = pybind11;
+
+namespace python_bindings {
+void BindCustomMetrics(pybind11::module_& main_module) {
+    auto metrics_module = main_module.def_submodule("metrics");
+
+    // These classes are used only in get_opts
+    py::class_<util::ICustomMetric, std::shared_ptr<util::ICustomMetric>>(metrics_module,
+                                                                          "CustomMetric");
+    py::class_<util::ICustomVectorMetric, std::shared_ptr<util::ICustomVectorMetric>>(
+            metrics_module, "CustomVectorMetrics");
+}
+}  // namespace python_bindings

src/python_bindings/py_util/py_to_any.cpp
@@ -1,6 +1,8 @@
 #include <pybind11/pybind11.h>
 
+#include <algorithm>
 #include <functional>
+#include <memory>
 #include <sstream>
 #include <stdexcept>
 #include <string>
@@ -36,6 +38,7 @@
 #include "core/util/enum_to_str.h"
 #include "python_bindings/py_util/create_dataframe_reader.h"
 #include "python_bindings/py_util/iterable_sequence_stream.h"
+#include "python_bindings/py_util/py_custom_metrics.h"
 
 namespace {
 
@@ -187,6 +190,30 @@ boost::any StringVectorToAny(std::string_view option_name, py::handle obj) {
     return out;
 }
 
+boost::any CustomMetricToAny(std::string_view, py::handle obj) {
+    return config::CustomMetricType{
+            new python_bindings::PyCustomMetric(py::reinterpret_borrow<py::object>(obj))};
+}
+
+boost::any CustomMetricsToAny(std::string_view option_name, py::handle obj) {
+    auto metric_handles = CastAndReplaceCastError<std::vector<py::handle>>(option_name, obj);
+    config::CustomMetricsType result(metric_handles.size());
+    std::ranges::transform(metric_handles, result.begin(),
+                           [](py::handle handle) -> config::CustomMetricType {
+                               if (handle.is_none()) {
+                                   return nullptr;
+                               }
+                               return std::make_shared<python_bindings::PyCustomMetric>(
+                                       py::reinterpret_borrow<py::object>(handle));
+                           });
+    return result;
+}
+
+boost::any CustomVectorMetricToAny(std::string_view, py::handle obj) {
+    return config::CustomVectorMetricType{
+            new python_bindings::PyCustomVectorMetric(py::reinterpret_borrow<py::object>(obj))};
+}
+
 std::unordered_map<std::type_index, ConvFunc> const kConverters{
         kNormalConvPair<bool>,
         kNormalConvPair<double>,
@@ -231,6 +258,9 @@ std::unordered_map<std::type_index, ConvFunc> const kConverters{
         kNormalConvPair<std::vector<std::string>>,
         kNormalConvPair<std::unordered_map<std::string, std::vector<unsigned int>>>,
         kNormalConvPair<std::shared_ptr<pac::model::IDomain>>,
+        {typeid(config::CustomMetricType), CustomMetricToAny},
+        {typeid(config::CustomMetricsType), CustomMetricsToAny},
+        {typeid(config::CustomVectorMetricType), CustomVectorMetricToAny},
 };
 
 }  // namespace