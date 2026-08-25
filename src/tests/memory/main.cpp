febe9758171216c08cdfe66348333251455eb20b
Petr Senichenkov <Petua41@yandex.ru>
2026-08-26 00:08:53 +0300

    Edit memory CMakeLists

--- src/tests/memory/main.cpp
+++ src/tests/memory/main.cpp
@@ -4,23 +4,18 @@
 #include "tests/common/all_csv_configs.h"
 #include "tests/memory/util.h"
 
-#include "core/algorithms/pac/pac_verifier/domain_pac_verifier/domain_pac_verifier.h"
-#include "core/algorithms/pac/model/idomain.h"
-#include "core/algorithms/pac/model/default_domains/ball.h"
+#include "core/algorithms/pac/pac_verifier/fd_pac_verifier/fd_pac_verifier.h"
 
 int main() {
    using namespace config::names;
    using namespace tests;
 
    algos::StdParamsMap options{
-             {kCsvConfig, kIowa5k},
-       {kColumnIndices, config::IndicesType{2, 3, 8, 9}},
-       {kDomain,
-        std::shared_ptr<pac::model::IDomain>(
-           new pac::model::Ball{std::vector<std::string>{"3", "Good Goods", "40", "John Doe"}, 15}
-        )
-       },
+             {kCsvConfig, kIowa20k},
+{kLhsIndices, config::IndicesType{2, 3}},
+{kRhsIndices, config::IndicesType{8, 9}},
+
    };
 
-  MeasuredRun<algos::pac_verifier::DomainPACVerifier>(options);
+  MeasuredRun<algos::pac_verifier::FDPACVerifier>(options);
 }
